06d0f2b7db69492238a4ad8fdaee45f1cf330acf
Expand-sys <expand@ur-mom.gay>
2022-08-14 22:14:26 +1000

    yeet

---
+++ go.mod
@@ -0,0 +1,16 @@
+module enemyfelled/main
+
+go 1.19
+
+require (
+	github.com/anthonynsimon/bild v0.13.0 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/image v0.0.0-20220722155232-062f8c9fd539 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"net/http"
+
+	"github.com/disintegration/imaging"
+	"github.com/fogleman/gg"
+	"github.com/gorilla/mux"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
+
+func main() {
+	text := "this is a test"
+	testimage := Request{"img.png", "Garamond.ttf", text, 255, 0, 0}
+	TextOnImg(testimage)
+
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+
+	// Bind to a port and pass our router in
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
+
+type Request struct {
+	BgImgPath  string
+	FontPath   string
+	Text       string
+	TextColorR uint8
+	TextColorG uint8
+	TextColorB uint8
+}
+
+func pxTopt(pt float64) (px float64) {
+	first := pt / 72.0
+	second := first * 96.0
+	return second
+}
+
+func TextOnImg(request Request) (image.Image, error) {
+	bgImage, err := gg.LoadImage(request.BgImgPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	imgWidth := bgImage.Bounds().Dx()
+	imgHeight := bgImage.Bounds().Dy()
+
+	dc := gg.NewContext(imgWidth, imgHeight)
+	dc.DrawImage(bgImage, 0, 0)
+	//rect := drawrect(request)
+	//dc.DrawImage(rect, imgWidth, imgHeight)
+
+	fontsize := pxTopt(float64(imgHeight) / 10.0)
+
+	if err := dc.LoadFontFace(request.FontPath, fontsize); err != nil {
+		fmt.Println(err)
+	}
+	x := float64(imgWidth / 2)
+	y := float64((imgHeight / 2) - int(fontsize)/2)
+
+	ac := gg.NewContext(imgHeight, imgWidth)
+	ac.DrawRectangle(0, 0, float64(imgHeight), float64(imgWidth))
+	grad := gg.NewLinearGradient(fontsize, 0, float64(imgHeight), 0)
+	grad.AddColorStop(0, color.RGBA{0, 0, 0, 0})
+	grad.AddColorStop(0.25, color.RGBA{0, 0, 0, 50})
+	grad.AddColorStop(0.35, color.RGBA{0, 0, 0, 150})
+	grad.AddColorStop(0.45, color.RGBA{0, 0, 0, 255})
+	grad.AddColorStop(0.5, color.RGBA{0, 0, 0, 255})
+	grad.AddColorStop(0.55, color.RGBA{0, 0, 0, 255})
+	grad.AddColorStop(0.65, color.RGBA{0, 0, 0, 150})
+	grad.AddColorStop(0.75, color.RGBA{0, 0, 0, 50})
+
+	grad.AddColorStop(1, color.RGBA{0, 0, 0, 0})
+	ac.SetFillStyle(grad)
+	ac.Fill()
+	//bc := DropShadow(ac.Image(), 50.0)
+	ac.SavePNG("temp.png")
+	cc := imaging.Rotate90(ac.Image())
+	dc.DrawImageAnchored(cc, imgWidth/2, imgHeight/2, 0.5, 0.5)
+
+	maxWidth := float64(imgWidth) - 60.0
+	dc.SetColor(color.RGBA{request.TextColorR, request.TextColorG, request.TextColorB, 255})
+	dc.DrawStringWrapped(request.Text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
+
+	if err := dc.LoadFontFace(request.FontPath, fontsize*1.05); err != nil {
+		fmt.Println(err)
+	}
+	dc.SetColor(color.RGBA{request.TextColorR, request.TextColorG, request.TextColorB, 150})
+	dc.DrawStringWrapped(request.Text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
+
+	dc.SavePNG("./out.png")
+	return dc.Image(), nil
+}
